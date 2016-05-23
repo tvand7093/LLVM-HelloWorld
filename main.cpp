036adcbbea38ff8576f08ccad01d0df15ce38e4e
Tyler Vanderhoef <tyler.j.vanderhoef@gmail.com>
2016-05-22 19:07:12 -0700

    Now builds a main function with a method called getThree.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "llvm/IR/Module.h"
+#include "llvm/IR/TypeBuilder.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/IR/CallingConv.h"
@@ -11,64 +12,110 @@
 #include "llvm/Support/type_traits.h"
 #include "llvm/Support/ToolOutputFile.h"
 #include "llvm/Support/FormattedStream.h"
+#include "llvm/Support/FileSystem.h"
 
-void doWork(){
-  // Have your "name" for the module set.
+using namespace llvm;
 
-  // Get the global LLVM context.
-  llvm::LLVMContext& ctx = llvm::getGlobalContext();
-
-  // Create a new module for our function.
-  llvm::Module* module = new llvm::Module("Hello World Module", ctx);
-
-  // Get the LLVM type for a 32-bit floating point value.
-  llvm::Type* f32
-    = llvm::Type::getPrimitiveType(ctx, llvm::Type::FloatTyID);
-
-  // Create a function type which returns a float, and takes
-  // no parameters.
-  llvm::FunctionType* ftype
-    = llvm::FunctionType::get(f32, false);
-
-  // Add a new function to the symbol table with the type that
-  // we created.
-  llvm::Constant* func
-    = module->getOrInsertFunction("two", ftype);
-
-  llvm::Function* realFunc = llvm::dyn_cast<llvm::Function>(func);
-  //check cast
-
-  // Create a single basic block for our function.
-  llvm::BasicBlock* basicBlock
-    = llvm::BasicBlock::Create(ctx, "", realFunc);
-
-
-  // Hardcode the return value for our function.
-  llvm::Value* retVal
-    = llvm::ConstantFP::get(f32, 2.0);
-
-  // Create the return instruction within our basic block.
-  llvm::ReturnInst* retInst
-    = llvm::ReturnInst::Create(ctx, retVal, basicBlock);
+void doMoreWork() {
+  LLVMContext TheContext;
+  std::unique_ptr<Module> TheModule =
+    llvm::make_unique<Module>("Hello LLVM!", TheContext);
+  IRBuilder<> Builder(TheContext);
   
-  //set filename up before this.
-  llvm::PassManager<llvm::Module> passes;
-  std::string error;
-
-  llvm::StringRef filename("MyFile.txt");
+  // Make the function type:  double(double,double) etc.
+  FunctionType *FT =
+    FunctionType::get(Type::getDoubleTy(TheContext), false);
   
-  llvm::tool_output_file *FDOut = new llvm::tool_output_file(filename, error);
-  llvm::formatted_raw_ostream outstream(FDOut->os());
+  Function *TheFunction =
+    Function::Create(FT, Function::ExternalLinkage, "getThree", TheModule.get());
 
-  passes.addPass(*(new llvm::DataLayout(module)));
-  passes.addPass(llvm::createPrintModulePass(&outstream));
-  passes.run(*module);
+  // Create a new basic block to start insertion into.
+  BasicBlock *BB = BasicBlock::Create(TheContext, "entry", TheFunction);
+  Builder.SetInsertPoint(BB);
 
-  FDOut->keep();
+  Value *RetVal = ConstantFP::get(TheContext, APFloat(3.0));
+
+  Builder.CreateRet(RetVal);
+
+  // Validate the generated code, checking for consistency.
+  verifyFunction(*TheFunction);
+
+  //now create the main function
+  FunctionType *mainFuncType =
+    FunctionType::get(Type::getInt32Ty(TheContext), false);
+
+  Function *mainFunc =
+    Function::Create(mainFuncType, Function::ExternalLinkage, "main", TheModule.get());
+
+  BasicBlock *mainBody = BasicBlock::Create(TheContext, "main", mainFunc);
+  Builder.SetInsertPoint(mainBody);
+
+  Value *mainReturn = ConstantInt::get(Type::getInt32Ty(TheContext), 0);
+
+  Builder.CreateRet(mainReturn);
+  verifyFunction(*mainFunc);
+  
+  TheModule->dump();
 }
 
 
+// void doWork(){
+//   // Have your "name" for the module set.
+
+//   // Get the global LLVM context.
+//   llvm::LLVMContext& ctx = llvm::getGlobalContext();
+
+//   // Create a new module for our function.
+//   llvm::Module* module = new llvm::Module("Hello World Module", ctx);
+
+//   // Get the LLVM type for a 32-bit floating point value.
+//   llvm::Type* f32
+//     = llvm::Type::getPrimitiveType(ctx, llvm::Type::FloatTyID);
+
+//   // Create a function type which returns a float, and takes
+//   // no parameters.
+//   llvm::FunctionType* ftype
+//     = llvm::FunctionType::get(f32, false);
+
+//   // Add a new function to the symbol table with the type that
+//   // we created.
+//   llvm::Constant* func
+//     = module->getOrInsertFunction("two", ftype);
+
+//   llvm::Function* realFunc = llvm::dyn_cast<llvm::Function>(func);
+//   //check cast
+
+//   // Create a single basic block for our function.
+//   llvm::BasicBlock* basicBlock
+//     = llvm::BasicBlock::Create(ctx, "block 1", realFunc);
+
+//   // Hardcode the return value for our function.
+//   llvm::Value* retVal
+//     = llvm::ConstantFP::get(f32, 2.0);
+
+//   // Create the return instruction within our basic block.
+//   llvm::ReturnInst* retInst
+//     = llvm::ReturnInst::Create(ctx, retVal, basicBlock);
+  
+//   //set filename up before this.
+//   llvm::PassManager<llvm::DataLayout> passes;
+//   std::error_code error;
+
+//   llvm::StringRef filename("MyFile.txt");
+  
+//   llvm::tool_output_file *FDOut = new llvm::tool_output_file(filename, 1);
+
+//   llvm::formatted_raw_ostream outstream(FDOut->os());
+
+//   passes.addPass(*(new llvm::DataLayout(module)));
+//   passes.addPass(llvm::createPrintModulePass(outstream));
+//   passes.run(*module);
+
+//   FDOut->keep();
+// }
+
+
 int main(int argc, char**argv) {
-  doWork();
+  doMoreWork();
   return 0;
 }
--- makefile
+++ makefile
@@ -1,8 +1,21 @@
 
 LLVM = `llvm-config --cxxflags --ldflags --libs --system-libs core`
 
-llvmtest:  main.cpp
+EXE = hello-llvm
+
+all: $(EXE)
+
+$(EXE): $(EXE).o
+	$(CC) $^ -o $@
+
+$(EXE).o: $(EXE).ir
+	llc -filetype=obj $^ -o $@
+
+$(EXE).ir: llvmtest
+	./$^ &> $@
+
+llvmtest: main.cpp
 	$(CXX) -g main.cpp $(LLVM) -o $@
 
 clean:
-	@rm -rf *.dSYM *~ \#*\# llvmtest
+	@rm -rf *.dSYM *~ \#*\# llvmtest *.o *.ir $(EXE)
