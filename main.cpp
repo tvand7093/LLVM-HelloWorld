ac961f4eb439bb6e860384ebee754914f4a18db2
Tyler Vanderhoef <tyler.j.vanderhoef@gmail.com>
2016-05-23 23:40:03 -0700

    Refactoring of file.

--- main.cpp
+++ main.cpp
@@ -6,29 +6,34 @@
 #include "llvm/IR/Verifier.h"
 #include "llvm/IR/IRPrintingPasses.h"
 #include "llvm/IR/IRBuilder.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/ADT/STLExtras.h"
 #include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/DataLayout.h"
+
+#include "llvm/ADT/STLExtras.h"
+
+#include "llvm/Bitcode/ReaderWriter.h"
+
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/type_traits.h"
 #include "llvm/Support/ToolOutputFile.h"
 #include "llvm/Support/FormattedStream.h"
 #include "llvm/Support/FileSystem.h"
-#include "llvm/Bitcode/ReaderWriter.h"
+
+#include <iostream>
 
 using namespace llvm;
 
-void printfMethod(Module *module, LLVMContext &context, IRBuilder<> &builder, Value* toPrint){
+void printfMethod(Module *module, LLVMContext &context, IRBuilder<> builder, Value* toPrint){
 
   if(!toPrint) return;
-  
+
   // accepts a char*, is vararg, and returns int
   std::vector<llvm::Type *> args;
   args.push_back(llvm::Type::getInt8PtrTy(context));
-  
+
   //create the entry in the text section.
   llvm::Value *formatStr = builder.CreateGlobalStringPtr("value = %d\n");
-  
+
   // get the method type, based on retrun value, given an arg list
   llvm::FunctionType *printfType =
     llvm::FunctionType::get(builder.getInt32Ty(), args, true);
@@ -39,37 +44,54 @@ void printfMethod(Module *module, LLVMContext &context, IRBuilder<> &builder, Va
 
   //set the values to pass into the printf call as parameters
   std::vector<llvm::Value *> values;
-  //just an integer 3 value
+  //set the printf arguement values
   values.push_back(formatStr);
   values.push_back(toPrint);
-  
+
   //add the printf call
   builder.CreateCall(printfFunc, values);
 }
 
-void doMoreWork(std::string outputFile) {
+Function* makeGetThreeFunc(Module *module, LLVMContext &ctx){
+  IRBuilder<> builder(ctx);
+
+  Type* intType = Type::getInt8Ty(ctx);
+  
+  FunctionType *funcType =
+    FunctionType::get(intType, false);
+  
+  Function *getThree =
+    Function::Create(funcType, Function::ExternalLinkage, "getThree", module);
+
+  // Create a new basic block to start insertion into.
+  BasicBlock *codeBlock = BasicBlock::Create(ctx, "entry", getThree);
+  builder.SetInsertPoint(codeBlock);
+  
+  Value *RetVal = ConstantInt::get(intType, 3);
+
+  builder.CreateRet(RetVal);
+
+  // Validate the generated code, checking for consistency.
+  verifyFunction(*getThree);
+  return getThree;
+}
+
+void outputIR(Module *module, std::string outputFile){
+  std::error_code EC;
+  StringRef path(outputFile);
+  raw_fd_ostream output(path, EC, sys::fs::F_None);
+
+  module->print(output, nullptr);
+}
+
+void makeProgram(std::string outputFile) {
   LLVMContext TheContext;
   std::unique_ptr<Module> TheModule =
     llvm::make_unique<Module>("Hello LLVM!", TheContext);
   IRBuilder<> Builder(TheContext);
-  
-  // Make the function type:  double(double,double) etc.
-  FunctionType *FT =
-    FunctionType::get(Type::getInt8Ty(TheContext), false);
-  
-  Function *TheFunction =
-    Function::Create(FT, Function::ExternalLinkage, "getThree", TheModule.get());
 
-  // Create a new basic block to start insertion into.
-  BasicBlock *BB = BasicBlock::Create(TheContext, "entry", TheFunction);
-  Builder.SetInsertPoint(BB);
-  
-  Value *RetVal = ConstantInt::get(Type::getInt8Ty(TheContext), 3);
-
-  Builder.CreateRet(RetVal);
-
-  // Validate the generated code, checking for consistency.
-  verifyFunction(*TheFunction);
+  //create the  getThree function
+  Function* getThree = makeGetThreeFunc(TheModule.get(), TheContext);
 
   //now create the main function
   FunctionType *mainFuncType =
@@ -78,28 +100,23 @@ void doMoreWork(std::string outputFile) {
   Function *mainFunc =
     Function::Create(mainFuncType, Function::ExternalLinkage, "main", TheModule.get());
 
+  //create the body of main
   BasicBlock *mainBody = BasicBlock::Create(TheContext, "main", mainFunc);
   Builder.SetInsertPoint(mainBody);
 
+  //set the return value of main
   Value *mainReturn = ConstantInt::get(Type::getInt32Ty(TheContext), 0);
 
   //get the value of calling the getThree function.
-  Function *CalleeF = TheModule->getFunction("getThree");
-
   std::vector<Value*> argValues;
-  Value* callThreeResult = Builder.CreateCall(CalleeF, argValues, "calltmp");
-  
+  Value* callThreeResult = Builder.CreateCall(getThree, argValues, "calltmp");
+
   //do a printf
   printfMethod(TheModule.get(), TheContext, Builder, callThreeResult);
-  
+
   Builder.CreateRet(mainReturn);
   verifyFunction(*mainFunc);
-
-  std::error_code EC;
-  StringRef path(outputFile);
-  raw_fd_ostream output(path, EC, sys::fs::F_None);
-
-  TheModule.get()->print(output, nullptr);
+  outputIR(TheModule.get(), outputFile);
 }
 
 int main(int argc, char**argv) {
@@ -108,6 +125,6 @@ int main(int argc, char**argv) {
   if(argc >= 2){
     outputFile = std::string(argv[1]);
   }
-  doMoreWork(outputFile);
+  makeProgram(outputFile);
   return 0;
 }
